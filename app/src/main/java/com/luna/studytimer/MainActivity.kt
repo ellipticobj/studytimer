be3d32795bfb46865b87ebcf571db831379dc401
nat <ellipticobj@gmail.com>
2025-01-05 01:08:59 +0800

    last commit

--- app/src/main/java/com/luna/studytimer/MainActivity.kt
+++ app/src/main/java/com/luna/studytimer/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.luna.studytimer
 
 import android.annotation.SuppressLint
@@ -13,30 +15,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -47,7 +42,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,10 +61,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-enum class Tab {
-    Pomodoro, TodoList
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -78,11 +68,12 @@ fun AppContent() {
     var studyTime by remember { mutableIntStateOf(25 * 60) }
     var breakTime by remember { mutableIntStateOf(5 * 60) }
     var timeLeft by remember { mutableIntStateOf(25 * 60) }
+    var totalTime by remember { mutableIntStateOf(25 * 60) }
     var isRunning by remember { mutableStateOf(false) }
+    var isPaused by remember { mutableStateOf(false) }
     var isStudySession by remember { mutableStateOf(true) }
     var sessionCount by remember { mutableIntStateOf(0) }
     var showSettings by remember { mutableStateOf(false) }
-    var currentTab by remember { mutableStateOf(Tab.Pomodoro) }
 
     LaunchedEffect(isRunning) {
         while (isRunning) {
@@ -93,9 +84,11 @@ fun AppContent() {
                 if (isStudySession) {
                     isStudySession = false
                     timeLeft = breakTime
+                    totalTime = breakTime
                 } else {
                     isStudySession = true
                     timeLeft = studyTime
+                    totalTime = studyTime
                     sessionCount++
                 }
             }
@@ -103,53 +96,41 @@ fun AppContent() {
     }
 
     Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = { showSettings = true }) {
-                Icon(Icons.Default.Settings, contentDescription = "Settings")
-            }
-        },
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = when (currentTab) {
-                            Tab.Pomodoro -> "Pomodoro"
-                            Tab.TodoList -> "Todo List"
-                        }
-                    )
-                }
+            MediumTopAppBar(
+                title = { Text("Pomodoro timer") },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
             )
-        },
-//        bottomBar = {
-//            BottomNavigationBar(currentTab) { tab ->
-//                currentTab = tab
-//            }
-//        }
+        }
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
+            verticalArrangement = Arrangement.Top
         ) {
+            Spacer(modifier = Modifier.height(30.dp))
+
             Text(
                 text = if (isStudySession) "Study time" else "Break time",
-                style = typography.headlineMedium,
-                modifier = Modifier.padding(16.dp)
+                style = typography.headlineSmall
             )
+
+            Spacer(modifier = Modifier.height(30.dp))
 
             Box(
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator(
-                    progress = { timeLeft.toFloat() / if (isStudySession) studyTime else breakTime },
+                    progress = { if (totalTime > 0) timeLeft.toFloat() / totalTime else 0f },
                     modifier = Modifier
-                        .size(290.dp)
-                        .offset(y = (-5).dp),
+                        .size(290.dp),
                     color = if (isStudySession) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
-                    strokeWidth = 12.dp,
-                    strokeCap = StrokeCap.Round
+                    strokeWidth = 12.dp
                 )
 
                 Text(
@@ -158,24 +139,139 @@ fun AppContent() {
                 )
             }
 
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            Spacer(modifier = Modifier.height(30.dp))
+            Text("total time: $totalTime")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
-                Button(onClick = { isRunning = !isRunning }) {
-                    Text(if (isRunning) "Pause" else "Start")
-                }
-                Spacer(modifier = Modifier.width(8.dp))
                 Button(
                     onClick = {
-                        if (isStudySession) {
-                            timeLeft = studyTime
+                        if (isRunning) {
+                            isRunning = false
+                            isPaused = true
                         } else {
-                            timeLeft = breakTime
+                            isRunning = true
+                            isPaused = false
                         }
-                        isRunning = false
-                    }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp)
+                        .height(60.dp),
+                    shape = RoundedCornerShape(
+                        topStart = 16.dp,
+                        topEnd = 16.dp,
+                        bottomStart = 4.dp,
+                        bottomEnd = 4.dp
+                    )
                 ) {
-                    Text("Reset")
+                    Text(if (isRunning) "Pause" else if (isPaused) "Resume" else "Start")
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                if (isRunning) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Button(
+                            onClick = {
+                                isRunning = false
+
+                                if (isStudySession) {
+                                    timeLeft = studyTime
+                                    totalTime = studyTime
+                                } else {
+                                    timeLeft = breakTime
+                                    totalTime = studyTime
+                                }
+
+                                isPaused = false
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .padding(start = 30.dp)
+                                .height(60.dp),
+                            shape = RoundedCornerShape(
+                                topStart = 4.dp,
+                                topEnd = 4.dp,
+                                bottomStart = 16.dp,
+                                bottomEnd = 4.dp
+                            ),
+                        ) {
+                            Text("Reset")
+                        }
+
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        Button(
+                            onClick = {
+                                timeLeft += 60
+                                totalTime += 60
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(end = 30.dp)
+                                .height(60.dp),
+                            shape = RoundedCornerShape(
+                                topStart = 4.dp,
+                                topEnd = 4.dp,
+                                bottomStart = 4.dp,
+                                bottomEnd = 16.dp
+                            )
+                        ) {
+                            Text("+1 min")
+                        }
+                    }
+                } else if (isPaused) {
+                    Button(
+                        onClick = {
+                            isRunning = false
+
+                            if (isStudySession) {
+                                timeLeft = studyTime
+                                totalTime = studyTime
+                            } else {
+                                timeLeft = breakTime
+                                totalTime = studyTime
+                            }
+
+                            isPaused = false
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp)
+                            .height(60.dp),
+                        shape = RoundedCornerShape(
+                            topStart = 4.dp,
+                            topEnd = 4.dp,
+                            bottomStart = 16.dp,
+                            bottomEnd = 16.dp
+                        ),
+                    ) {
+                        Text("Reset")
+                    }
+                } else {
+                    Button(
+                        onClick = { showSettings = true },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 30.dp, end = 30.dp)
+                            .height(60.dp),
+                        shape = RoundedCornerShape(
+                            topStart = 4.dp,
+                            topEnd = 4.dp,
+                            bottomStart = 16.dp,
+                            bottomEnd = 16.dp
+                        ),
+                    ) {
+                        Text("Change time")
+                    }
                 }
             }
 
@@ -204,94 +300,71 @@ fun AppContent() {
     ) {
 
     }
-//        when (currentTab) {
-//            Tab.Pomodoro -> PomodoroScreen()
-//            Tab.TodoList -> TodoListScreen()
-//        }
-//    }
-//}
+}
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun SettingsBottomSheet(
-        studyTime: Int,
-        breakTime: Int,
-        onStudyTimeChange: (Int) -> Unit,
-        onBreakTimeChange: (Int) -> Unit,
-        onClose: () -> Unit
+
+@Composable
+fun SettingsBottomSheet(
+    studyTime: Int,
+    breakTime: Int,
+    onStudyTimeChange: (Int) -> Unit,
+    onBreakTimeChange: (Int) -> Unit,
+    onClose: () -> Unit
+) {
+    ModalBottomSheet(
+        onDismissRequest = onClose,
+        sheetState = rememberModalBottomSheetState(),
+        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
     ) {
-        ModalBottomSheet(
-            onDismissRequest = onClose,
-            sheetState = rememberModalBottomSheetState(),
-            shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .imePadding(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("Settings", style = typography.headlineMedium)
+            Text("Settings", style = typography.headlineMedium)
 
-                Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("Study time (mins)", fontSize = 14.sp)
-                    TextField(
-                        value = (studyTime / 60).toString(),
-                        onValueChange = {
-                            val newValue = it.toIntOrNull() ?: 25
-                            onStudyTimeChange(newValue * 60)
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        singleLine = true
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("Break time (mins)", fontSize = 14.sp)
-                    TextField(
-                        value = (breakTime / 60).toString(),
-                        onValueChange = {
-                            val newValue = it.toIntOrNull() ?: 5
-                            onBreakTimeChange(newValue * 60)
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        singleLine = true
-                    )
-                }
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Study time (mins)", fontSize = 14.sp)
+                TextField(
+                    value = (studyTime / 60).toString(),
+                    onValueChange = {
+                        val newValue = it.toIntOrNull() ?: 25
+                        onStudyTimeChange(newValue * 60)
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    singleLine = true
+                )
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(
-                onClick = onClose,
-            ) {
-                Text("Save settings")
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Break time (mins)", fontSize = 14.sp)
+                TextField(
+                    value = (breakTime / 60).toString(),
+                    onValueChange = {
+                        val newValue = it.toIntOrNull() ?: 5
+                        onBreakTimeChange(newValue * 60)
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    singleLine = true
+                )
             }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = onClose
+        ) {
+            Text("Save settings")
         }
     }
 }
-
-//@Composable
-//fun BottomNavigationBar(currentTab: Tab, onTabSelected: (Tab) -> Unit) {
-//    NavigationBar {
-//        NavigationBarItem(
-//            selected = currentTab == Tab.Pomodoro,
-//            onClick = { onTabSelected(Tab.Pomodoro) },
-//            label = { Text("Pomodoro Timer") },
-//            icon = { Icon(Icons.Outlined.Add, contentDescription = "Pomodoro Timer")}
-//        )
-//        NavigationBarItem(
-//            selected = currentTab == Tab.TodoList,
-//            onClick = { onTabSelected(Tab.TodoList) },
-//            label = { Text("Todo List") },
-//            icon = { Icon(Icons.Default.List, contentDescription = "Todo List") }
-//        )
-//    }
-//}
 
 @Preview(showBackground = true)
 @Composable
